673f8cced75c9ae7c6905f6b790bcfc113a98d1a
rycahayash <rycahaya.205025@mhs.its.ac.id>
2023-04-05 15:21:00 +0700

    UI App

---
+++ MoneyConverter/MoneyConverter/Form1.cs
@@ -0,0 +1,35 @@
+namespace MoneyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
